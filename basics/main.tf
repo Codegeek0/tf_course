06dd24b7c01d4a496d329f07a88441edc9c4476b
Hemanth Kumar <hemanthkumar@gmail.com>
2026-01-19 18:41:54 +0530

    initail changes

---
+++ basics/main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-07ff62358b87c7116"  # Specify an appropriate AMI ID
+    instance_type = "t3.micro"
+    key_name   = "learning"
+}
