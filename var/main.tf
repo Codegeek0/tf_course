078aca55c6def074c83d1f0d6ddfb23455d13fc1
Hemanth Kumar <hemanthkumar@gmail.com>
2026-01-19 19:14:27 +0530

    var addition

---
+++ var/main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+variable "ami_id" {
+    description = "The AMI ID for the EC2 instance"
+    type        = string
+    default     = "ami-07ff62358b87c7116"  # Default AMI ID
+  
+}
+
+variable "instance_type" {
+    description = "The type of instance to use"
+    type        = string
+    default     = "t3.micro"
+  
+}
+
+variable "key_value" {
+    description = "The key name for the EC2 instance"
+    type        = string
+    default     = "learning"
+  
+}
+
+resource "aws_instance" "example" {
+    ami           = var.ami_id  # Specify an appropriate AMI ID
+    instance_type = var.instance_type
+    key_name   = var.key_value
+}
